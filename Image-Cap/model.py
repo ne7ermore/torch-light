fix: use log softmax in cost function

Image-Cap/model.py
@@ -10,6 +10,22 @@ import numpy as np
 
 from const import BOS, PAD
 
+class RewardCriterion(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, props, words, scores):
+        assert words.size() == scores.size()
+
+        mask = (words > 0).float()
+        masked_ss = scores*mask
+
+        output = masked_ss*props
+        output = torch.sum(output) / torch.sum(mask)
+        reward = torch.sum(masked_ss) / torch.sum(mask)
+
+        return output, reward
+
 class Attention(nn.Module):
     def __init__(self, hsz):
         super().__init__()
@@ -102,20 +118,22 @@ class Actor(nn.Module):
         emb_enc = self.lookup_table(word)
         hiddens = [hidden[0].squeeze()]
         attn = torch.transpose(hidden[0], 0, 1)
-        outputs, words = [], []
+        outputs, words, sample_props = [], [], []
 
         for _ in range(self.max_len):
             _, hidden = self.rnn(torch.cat([emb_enc, attn], -1), hidden)
-            props = self.out(hidden[0][-1])
+            props = F.log_softmax(self.out(hidden[0][-1]), dim=-1)
             attn = self.attn(hiddens, hidden[0][-1])
 
-            _, word = torch.max(props, -1, keepdim=True)
+            _props = props.data.clone().exp()
+            word = Variable(_props.multinomial(1), requires_grad=False)
             emb_enc = self.lookup_table(word)
 
             words.append(word)
             outputs.append(props.unsqueeze(1))
+            sample_props.append(props.gather(1, word))
 
-        return F.log_softmax(torch.cat(outputs, 1), dim=-1), torch.cat(words, 1)
+        return F.log_softmax(torch.cat(outputs, 1), dim=-1), torch.cat(words, 1), torch.cat(sample_props, 1)
 
     def _reset_parameters(self):
         stdv = 1. / math.sqrt(self.vocab_size)

Image-Cap/rouge.py
@@ -49,7 +49,7 @@ def rouge_l(evals, refs):
 
     scores = np.asarray(scores, dtype=np.float32)
     scores = np.repeat(scores[:, np.newaxis], evals.shape[1], 1)
-    scores = Variable(torch.from_numpy(scores))
+    scores = Variable(torch.from_numpy(scores), requires_grad=False)
 
     if use_cuda: scores = scores.cuda()
 
@@ -70,14 +70,31 @@ def mask_score(props, words, scores):
 if __name__ == '__main__':
     import torch
     from torch.autograd import Variable
+    import torch.nn.functional as F
+    from model import RewardCriterion
+
     data = Variable(torch.LongTensor([[3,1,2,3,1,0],[2,3,4,4,0,0]]))
     label = Variable(torch.LongTensor([[3,1,2,3,1,0],[2,3,2,3,1,0]]))
+    bl = Variable(torch.LongTensor([[3,1,2,3,2,0],[1,3,4,4,0,0]]))
     data = data.cuda()
     label = label.cuda()
-    print(rouge_l(data, label))
+    bl = bl.cuda()
+
+    reward = rouge_l(bl, label) - rouge_l(data, label)
+    print(reward)
 
     props = torch.randn(16,17,256)
     words = torch.LongTensor([[i for i in range(16, -1, -1)] for _ in range(16)])
     scores = torch.randn(16,17)
 
     print(mask_score(props, words, scores))
+
+    crit = RewardCriterion()
+    crit = crit.cuda()
+
+    props = F.log_softmax(Variable(torch.randn(2,6,256), requires_grad=True).cuda(), dim=-1)
+    max_props, _ = torch.max(props, -1)
+
+    loss, reward = crit(max_props, data, reward)
+    print(loss)
+    print(reward)

Image-Cap/train.py
@@ -1,6 +1,7 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='Image Cation')
+parser.add_argument('--logdir', type=str, default='tb_logdir')
 parser.add_argument('--seed', type=int, default=1111)
 parser.add_argument('--unuse_cuda', action='store_true')
 parser.add_argument('--path', type=str, default='data/')
@@ -27,6 +28,23 @@ use_cuda = torch.cuda.is_available() and not args.unuse_cuda
 
 if use_cuda:
     torch.cuda.manual_seed(args.seed)
+
+# ##############################################################################
+# Tensorboard
+################################################################################
+try:
+    import tensorflow as tf
+    global tf_step;tf_step = 0
+except ImportError:
+    tf = None
+
+tf_summary_writer = tf and tf.summary.FileWriter(args.logdir)
+
+def add_summary_value(key, value):
+    global tf_step
+
+    summary = tf.Summary(value=[tf.Summary.Value(tag=key, simple_value=value)])
+    tf_summary_writer.add_summary(summary, tf_step)
 
 # ##############################################################################
 # Load data
@@ -86,6 +104,7 @@ optim_C = Optim(critic.parameters(), args.lr, False, args.new_lr, args.grad_clip
 
 criterion_A = torch.nn.CrossEntropyLoss(ignore_index=PAD)
 criterion_C = torch.nn.MSELoss()
+criterion_AC = model.RewardCriterion()
 
 if use_cuda:
     actor = actor.cuda()
@@ -109,6 +128,7 @@ def fix_variable(varias):
 
 def pre_train_actor():
     total_loss = 0.
+    global tf_step
     for imgs, labels in tqdm(training_data,
             mininterval=1, desc="Pre-train Actor", leave=False):
         optim_pre_A.zero_grad()
@@ -123,6 +143,12 @@ def pre_train_actor():
         optim_pre_A.clip_grad_norm()
         optim_pre_A.step()
         total_loss += loss.data
+        if tf is not None:
+            add_summary_value("pre-train actor loss", loss.data[0])
+            tf_step += 1
+
+            if tf_step % 100 == 0:
+                tf_summary_writer.flush()
 
     return total_loss[0]/training_data.sents_size
 
@@ -130,6 +156,7 @@ def pre_train_critic():
     iterations, total_loss = 0, .0
     actor.eval()
     critic.train()
+    global tf_step
     for imgs, labels in tqdm(training_data,
             mininterval=1, desc="Pre-train Critic", leave=False):
         optim_pre_C.zero_grad()
@@ -155,6 +182,12 @@ def pre_train_critic():
         optim_pre_C.step()
 
         iterations += 1
+        if tf is not None:
+            add_summary_value("pre-train critic loss", loss.data[0])
+            tf_step += 1
+
+            if tf_step % 100 == 0:
+                tf_summary_writer.flush()
 
         if iterations == args.iterations: break
 
@@ -164,6 +197,7 @@ def train_actor_critic():
     loss_A = loss_C = .0
     actor.train()
     critic.train()
+    global tf_step
 
     for imgs, labels in tqdm(training_data,
             mininterval=1, desc="Actor-Critic Training", leave=False):
@@ -190,13 +224,22 @@ def train_actor_critic():
         optim_C.clip_grad_norm()
         optim_C.step()
 
-        _g = mask_score(props_A, words_A[:, 1:], scores_A-scores_C)
-        loss_a = criterion_A(_g, labels.view(-1))
+        _, sample_words, sample_props = actor.speak(hidden_A)
+        loss_a, reward = criterion_AC(sample_props, sample_words, scores_C-scores_A)
         loss_a.backward()
         loss_A += loss_a.data
 
         optim_A.clip_grad_norm()
         optim_A.step()
+
+        if tf is not None:
+            add_summary_value("train critic loss", loss_c.data[0])
+            add_summary_value("train actor loss", loss_a.data[0])
+            add_summary_value("train actor reward", reward.data[0])
+            tf_step += 1
+
+            if tf_step % 100 == 0:
+                tf_summary_writer.flush()
 
     loss_A = loss_A[0]/training_data.sents_size
     loss_C = loss_C[0]/training_data.sents_size
@@ -211,7 +254,7 @@ def eval():
         enc = actor.encode(imgs)
 
         hidden = actor.feed_enc(enc)
-        props, words = actor.speak(hidden)
+        props, words, _ = actor.speak(hidden)
 
         loss = criterion_A(props.view(-1, props.size(2)), labels.view(-1))
         scores = rouge_l(words, labels)
@@ -229,6 +272,7 @@ try:
     s_time = time.time()
     print("="*40 + "Pre-train Actor" + "="*40)
     actor.train()
+    tf_step = 0
     for step in range(args.actor_epochs):
         loss = pre_train_actor()
         print("-"*20 + "epoch-{} | loss: {:.4f} | time: {:2.2f}".format(step, loss, time.time()-s_time) + "-"*20)
@@ -246,11 +290,13 @@ try:
         }
         torch.save(model_source, args.save.format("pret-actor_" + str(step)))
 
+    tf_step = 0
     print("="*40 + "Pre-train Critic" + "="*40)
     loss = pre_train_critic()
     print("-"*20 + "pre-train critic | loss: {:.4f} | time: {:2.2f}".format(loss, time.time()-s_time) + "-"*20)
     s_time = time.time()
 
+    tf_step = 0
     print("="*40 + "Actor-Critic Training" + "="*40)
     for step in range(args.epochs):
         loss_A, loss_C = train_actor_critic()